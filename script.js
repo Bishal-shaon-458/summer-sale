dbae7db3f5ea0f8953f8cfa8d2a51be160e2675d
Bishal-shaon-458 <shawonsam168@gmail.com>
2023-10-10 07:12:40 +0100

    all done

---
+++ script.js
@@ -0,0 +1,8 @@
+let product = document.getElementById('select-item');
+let copunIntpuFild = document.getElementById('cupon-input')
+
+
+function addToCard(target){
+    let getItemName = target.childNodes[3];
+    console.log(getItemName)
+}
